62058ef7f74e32cc18d026988349d824c85d2e95
Daniel Windsor <dw.accounts@protonmail.com>
2020-04-24 10:04:21 +1200

    Update references

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const express = require('express')
 const server = express()
 
 server.use(express.json())
-server.use(express.static(path.join(__dirname, './public')))
+server.use(express.static('public'))
 
 server.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/../public/index.html'))
