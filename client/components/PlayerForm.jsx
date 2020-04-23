0337448c952e3ba74deafd422bb9f4ac5fb096e5
Daniel Windsor <dw.accounts@protonmail.com>
2020-04-24 10:43:12 +1200

    Style player form

--- client/components/PlayerForm.jsx
+++ client/components/PlayerForm.jsx
@@ -31,20 +31,21 @@ class PlayerForm extends React.Component {
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label htmlFor="player1">Player One: </label>
-        <input type='text' name='player1' onChange={this.handleChange} />
-
-        <label htmlFor="player2">Player Two: </label>
-        <input type='text' name='player2' onChange={this.handleChange} />
-
-        <label htmlFor="player3">Player Three: </label>
-        <input type='text' name='player3' onChange={this.handleChange} />
-
-        <label htmlFor="player4">Player Four: </label>
-        <input type='text' name='player4' onChange={this.handleChange} />
-
-        <input type='submit' value='Play!' onSubmit={this.handleSubmit} />
+      <form className='playerForm' onSubmit={this.handleSubmit}>
+        <h2>Who is playing?</h2>
+        <label htmlFor="player1">Player 1: 
+          <input type='text' name='player1' onChange={this.handleChange} />
+        </label>
+        <label htmlFor="player2">Player 2: 
+          <input type='text' name='player2' onChange={this.handleChange} />
+        </label>
+        <label htmlFor="player3">Player 3: 
+          <input type='text' name='player3' onChange={this.handleChange} />
+        </label>
+        <label htmlFor="player4">Player 4: 
+          <input type='text' name='player4' onChange={this.handleChange} />
+        </label >
+        <input id='submit' type='submit' value='Play!' onSubmit={this.handleSubmit} />
       </form>
     )
   }
