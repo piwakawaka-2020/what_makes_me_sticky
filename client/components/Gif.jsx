b69455d4bd68997b16cadf8521c9734e287bcdec
tcodling <thomas.codling@outlook.com>
2020-04-24 11:03:44 +1200

    get git drag n drop kinda working

--- client/components/Gif.jsx
+++ client/components/Gif.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
 
 const Gif = () => {
-  return (
-    <img className='gif' src='giphy.gif'/>
+    const dragStart = e => {
+        const target = e.target
+        e.dataTransfer.setData('gifId', target.id)
+
+        setTimeout(() => {
+            target.style.display = 'none'
+        }, 0)
+    }
+
+    const dragOver = e => {
+        e.stopPropagation();
+    }
+    
+    return (
+        <img 
+            id='giphy.gif'
+            className='gif' 
+            src='giphy.gif'
+            onDragStart={dragStart}
+            onDragOver={dragOver}
+            draggable='true'
+        />
   )
 }
 
--- client/components/SelectionArea.jsx
+++ client/components/SelectionArea.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 
 const SelectionArea = () => {
-  return (
-    <div id='selection'></div>
-  )
+    const drop = e => {
+        e.preventDefault();
+
+        const gifId = e.dataTransfer.getData('gifId')
+        const gif = document.getElementById(gifId)
+
+        e.target.appendChild(gif)
+    }
+
+    const dragOver = e => {
+        e.preventDefault()
+    }
+
+    return (
+        <div id='selection'
+            onDrop={drop}
+            onDragOver={dragOver}
+        ></div>
+    )
 }
 
 export default SelectionArea
