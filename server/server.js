d2b1c3a5a556419c2f34a921512b76d6bceb7238
Daniel Windsor <dw.accounts@protonmail.com>
2020-04-24 09:49:42 +1200

    Add route to render index

--- server/server.js
+++ server/server.js
@@ -3,7 +3,14 @@ const express = require('express')
 
 const server = express()
 
+const gameRoute = require('./routes/game')
+
 server.use(express.json())
 server.use(express.static(path.join(__dirname, './public')))
 
+server.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/../public/index.html'))
+})
+
 module.exports = server
+
