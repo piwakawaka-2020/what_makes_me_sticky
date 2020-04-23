4e269c704fdfa911388ed506e8c869de9be740a9
tcodling <thomas.codling@outlook.com>
2020-04-24 10:03:23 +1200

    add components with some style

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 
+import GifList from './GifList'
+
 const App = () => {
   return (
+    <>
     <h1>React development has begun!</h1>
+    <GifList />
+    </>
   )
 }
 
---
+++ client/components/Gif.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Gif = () => {
+  return (
+    <div className='gif'></div>
+  )
+}
+
+export default Gif
