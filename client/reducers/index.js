c87e1721a2cfbe84bc3a4db3f2137241c6885f89
simon-hall-970 <60405692+simon-hall-970@users.noreply.github.com>
2020-04-24 12:01:23 +1200

    Update index.js

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -3,12 +3,9 @@ import {combineReducers} from 'redux'
 // Import your function here'
 
 import gifs from './gifs'
-
-export default combineReducers({
-    gifs
-
 import players from './players'
 
 export default combineReducers({
-  players
+    gifs,
+    players
 })
