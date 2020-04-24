9de377d6782a8f6e59ac7d06a5d0e4138f154cfb
Daniel Windsor <dw.accounts@protonmail.com>
2020-04-24 12:58:05 +1200

    Add score box toggle

--- client/reducers/players.js
+++ client/reducers/players.js
@@ -1,11 +1,11 @@
 const players = (state = [], action) => {
-  switch(action.type) {
-    case 'SET_PLAYERS' :
+  switch (action.type) {
+    case 'SET_PLAYERS':
       return action.players
 
-    case 'SET_ACTIVE_PLAYER' :
+    case 'SET_ACTIVE_PLAYER':
       return action.players.map(player => {
-        if(player.id === action.id) {
+        if (player.id === action.id) {
           player.isActive = true
         } else {
           player.isActive = false
@@ -13,10 +13,19 @@ const players = (state = [], action) => {
 
         return player
       })
-    
-    default: 
+
+    case 'UPDATE_SCORE':
+      return state.map(player => {
+        if (player.id == action.id) {
+          player.score += 1
+        }
+        console.log(player)
+        return player
+      })
+
+    default:
       return state
-    }
+  }
 }
 
 export default players
