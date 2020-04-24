4a06cf87c6f9a396e5f8ae34430a531f861e51e3
Daniel Windsor <dw.accounts@protonmail.com>
2020-04-24 13:37:18 +1200

    Change player at turn end

--- client/components/Score.jsx
+++ client/components/Score.jsx
@@ -1,17 +1,24 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { updateScore } from '../actions/players'
+import { updateScore, setActivePlayer } from '../actions/players'
 import { showScore } from '../actions/score'
 
 class Score extends React.Component {
 
   givePoints = e => {
     this.props.dispatch(updateScore(this.props.id))
-    this.props.dispatch(showScore(false))
+    this.changePlayer()
   }
 
-  noPoints = e => {
+  changePlayer() {
     this.props.dispatch(showScore(false))
+
+    let newId = this.props.id + 1
+
+    if (newId > this.props.playerNum - 1) {
+      newId = 0
+    }
+    this.props.dispatch(setActivePlayer(this.props.players, newId))
   }
 
   render() {
@@ -19,7 +26,7 @@ class Score extends React.Component {
       <div className='score'>
         <h1>Give {this.props.name} a score</h1>
         <button onClick={this.givePoints}>Amazing</button>
-        <button onClick={this.noPoints}>Rubbish</button>
+        <button onClick={this.changePlayer}>Rubbish</button>
       </div>
 
     )
@@ -28,10 +35,13 @@ class Score extends React.Component {
 
 function mapStateToProps(globalState) {
   const activePlayer = globalState.players.filter(player => player.isActive)
+  // const playerNum = globalState.players.length
 
   console.log()
   return {
-    ...activePlayer[0]
+    ...activePlayer[0],
+    players: globalState.players,
+    playerNum: globalState.players.length
   }
 }
 
